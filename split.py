a8a3356c6e2591c4b06fc067d323b7cf475c0c28
Leho <leho@leho.dev>
2021-07-09 14:20:39 +1000

    Add Username Extractor

---
+++ split.py
@@ -0,0 +1,14 @@
+# Made By Leho - github.com/Lehoooo
+import re
+print("Starting Username Extracter - Made By github.com/Lehoooo")
+
+passwordfile = open("combo.txt", "r")
+outputfile = open("usernames.txt", "w")
+pass_split = passwordfile.read().split("\n")
+
+for x in range(len(pass_split)):
+    usernamesplit = re.split(f':|\n', pass_split[int(x)])
+    outputfile.write(str(usernamesplit[2]) + "\n")
+    print(usernamesplit[2])
+
+outputfile.close()
